2be1d6a353aca7db67e39d7bc35e15932f3ffc0e
cmontoyam <65821196+cmontoyam@users.noreply.github.com>
2020-06-13 15:47:22 -0400

    yak-prueba

---
+++ indexold.php
@@ -0,0 +1,5 @@
+
+<!-- saved from url=(0029)http://172.105.92.217/prueba/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body><h1>Siempre detonado!!!!!</h1>
+
+</body></html>
